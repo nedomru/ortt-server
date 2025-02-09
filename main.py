8a32b8034cd4961552e3a3c7b0ac1470531bcd68
Roman Chursanov <authfailed@gmail.com>
2025-02-09 13:04:14 +0500

    Откат кастомных ключей для указания на стороне клиента

--- main.py
+++ main.py
@@ -256,13 +256,7 @@ async def handle_send_command(request):
                 status=400
             )
 
-        send_command = ""
-        match command:
-            case "ping":
-                send_command = "ping -n 30 -l 1200"
-            case "tracert": 
-                send_command = "tracert /d /4"
-        result = await request.app['server'].send_command(client_id, send_command, target)
+        result = await request.app['server'].send_command(client_id, command, target)
         return web.json_response(result)
     except Exception as e:
         return web.json_response(
